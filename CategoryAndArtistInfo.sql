800508af55745b07b405b9564f687320b5571d53
Dylan <dtetheridge@gmail.com>
2019-02-21 10:53:04 +1300

    Category and Artist Information Commit
    All the data for category and artist queries.

---
+++ CategoryAndArtistInfo.sql
@@ -0,0 +1,23 @@
+use `DE-AK`;
+
+INSERT INTO category
+	VALUES
+		((categoryID), "Arts", "Biography"),
+        ((categoryID), "Arts", "Painting"),
+        ((categoryID), "Mathematics", "Theorem"),
+        ((categoryID), "Mathematics", "Biography"),
+        ((categoryID), "Mathematics", "Algorithm"),
+        ((categoryID), "Technology", "Biography"),
+        ((categoryID), "Technology", "Programming");
+        
+INSERT INTO artist
+	VALUES
+		((artistID), "Claude Monet", "1840", "1926", "French", "Painter"),
+        ((artistID), "Leonardo da Vinci", "1452", "1519", "Italian", "Art and Science"),
+        ((artistID),  "Mona Lisa", "", "", "", ""),
+        ((artistID), "Pythagorean theorem", "", "", "", ""),
+        ((artistID), "Euclid", "4th century BC", "3rd centry BC", "", ""),
+        ((artistID), "Quicksort", "", "", "", ""),
+        ((artistID), "Bill Gates", "1955", "", "USA", "Founder of the Microsoft Corporation"),
+        ((artistID), "Steve Jobs", "1955", "2011", "USA", "Co-creator of the Macintosh, iPod, iPhone, iPad, and first Apple Stores"),
+        ((artistID), "Java", "", "", "", "");
